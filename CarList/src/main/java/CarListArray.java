9e595172e12ef5096f20bbf4fce2d4e6252bfcfb
Den_Yeromenko <den.yeromenko@gmail.com>
2021-01-02 14:43:36 +0200

    The example of arraylist was added.It is registration of cars

---
+++ CarList/src/main/java/CarListArray.java
@@ -0,0 +1,3 @@
+public class CarListArray implements CarList {
+
+}
---
+++ CarList/src/test/java/CarListTest.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class CarListTest {
+    public Car car;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    
+}
