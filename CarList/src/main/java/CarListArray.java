5302a17168691e4770e3203e23a771523ada9e7b
Den_Yeromenko <den.yeromenko@gmail.com>
2021-01-02 23:40:18 +0200

    Tests was created

--- CarList/src/main/java/CarListArray.java
+++ CarList/src/main/java/CarListArray.java
@@ -1,3 +1,31 @@
 public class CarListArray implements CarList {
+    @Override
+    public Car get(int index) {
 
+    }
+
+    @Override
+    public void add(Car car) {
+
+    }
+
+    @Override
+    public boolean remove(Car car) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAt(int index) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
 }
