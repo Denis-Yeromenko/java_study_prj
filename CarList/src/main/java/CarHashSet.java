7b43cc0ac8e63c398acbbf6214ebc1af33244d0f
Den_Yeromenko <den.yeromenko@gmail.com>
2021-01-08 17:03:05 +0200

    Some bugs are fixed

--- CarList/src/main/java/CarHashSet.java
+++ CarList/src/main/java/CarHashSet.java
@@ -1,23 +1,33 @@
 public class CarHashSet implements CarSet {
     private static final int INITIAL_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75;
     private int size = 0;
     private static Entry[] array = new Entry[INITIAL_CAPACITY];
 
     @Override
     public boolean add(Car car) {
-        int position = getElementPosition(car, array.length);
-        if (array[position] == null) {
-            array[position] = new Entry(car, null);
+        if(size >= array.length * LOAD_FACTOR){
+            increaseArray();
+        }
+        boolean added = add(car, array);
+        if(added){
             size++;
+        }
+        return added;
+    }
+
+    public boolean add(Car car, Entry[] dest){
+        int position = getElementPosition(car, dest.length);
+        if (dest[position] == null) {
+            dest[position] = new Entry(car, null);
             return true;
         } else {
-            Entry existedElement = array[position];
+            Entry existedElement = dest[position];
             while (true) {
                 if (existedElement.value.equals(car)) {
                     return false;
                 } else if (existedElement.next == null) {
                     existedElement.next = new Entry(car, null);
-                    size++;
                     return true;
                 } else {
                     existedElement = existedElement.next;
@@ -62,6 +72,17 @@ public class CarHashSet implements CarSet {
         size = 0;
         array = new Entry[INITIAL_CAPACITY];
 
+    }
+
+    private void increaseArray() {
+        Entry[] newArray = new Entry[array.length * 2];
+        for(Entry entry: array){
+            Entry existedElement = entry;
+            while (existedElement!=null){
+                add(existedElement.value, newArray);
+                existedElement = existedElement.next;
+            }
+        }
     }
 
     private int getElementPosition(Car car, int arrayLength) {
