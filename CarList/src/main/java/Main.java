7677be27b98d195835d0d493b91a91264b827aae
Den_Yeromenko <den.yeromenko@gmail.com>
2021-01-11 18:10:30 +0200

    The comparable method was overriding

---
+++ CarList/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> numbers = new TreeSet<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o1 > o2) {
+                    return -1;
+                } else if (o1 == o2) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
+        });
+        for (int i = 0; i < 100; i++) {
+            numbers.add((int) (Math.random() * 10));
+        }
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
