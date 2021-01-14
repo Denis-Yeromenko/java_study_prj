bfcc17c4392f21be1e320e19545c47951735f6ec
Den_Yeromenko <den.yeromenko@gmail.com>
2021-01-14 22:56:52 +0200

    The interface CarMap and class CarOwner was added

---
+++ CarList/src/main/java/CarOwner.java
@@ -0,0 +1,35 @@
+public class CarOwner {
+    private int id;
+    private String name;
+    private String lastname;
+
+    public CarOwner(int id, String name, String lastname) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
