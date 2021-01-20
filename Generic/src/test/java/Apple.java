0dd11070c0219492377990bec3ab51068adab9ea
Den_Yeromenko <den.yeromenko@gmail.com>
2021-01-20 14:34:31 +0200

    Fruits, Apple and Orange was added

---
+++ Generic/src/test/java/Apple.java
@@ -0,0 +1,10 @@
+public class Apple extends Fruit{
+    private final int WEIGHT = 1;
+
+    public Apple() {
+    }
+
+    public int getWEIGHT() {
+        return WEIGHT;
+    }
+}
---
+++ Generic/src/test/java/Orange.java
@@ -0,0 +1,11 @@
+public class Orange extends Fruit {
+    private final double WEIGHT = 1.5;
+
+    public Orange() {
+
+    }
+
+    public double getWEIGHT() {
+        return WEIGHT;
+    }
+}
