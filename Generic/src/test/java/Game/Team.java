5211ec3b500b28edf3e884919fbf37707cd7c751
Den_Yeromenko <den.yeromenko@gmail.com>
2021-01-22 22:21:52 +0200

    The game was fixed (in Team class)

--- Generic/src/test/java/Game/Team.java
+++ Generic/src/test/java/Game/Team.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Team {
+public class Team<T extends Participant> {
     private String name;
-    private List<Participant> participants = new ArrayList<>();
+    private List<T> participants = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
     }
 
-    public void newParticipantAdd(Participant p){
-        participants.add(p);
-        System.out.println(p.getName() + " was added in " + name +" team");
+    public void newParticipantAdd(T participant){
+        participants.add(participant);
+        System.out.println((participant).getName() + " was added in " + name +" team");
     }
 
-    public void playWith(Team team){
+    public void playWith(Team<T> team){
         String winnerName;
         Random random = new Random();
         int i = random.nextInt(2);
