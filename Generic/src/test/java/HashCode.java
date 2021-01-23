640326403de2c8f95f3121713c722cf1fc188ec5
Den_Yeromenko <den.yeromenko@gmail.com>
2021-01-23 21:43:20 +0200

    The example of hashcode was added

---
+++ Generic/src/test/java/HashCode.java
@@ -0,0 +1,12 @@
+import java.util.Objects;
+
+public class HashCode {
+    public static void main(String[] args) {
+        Car car1 = new Car("BMW", 1);
+        Car car2 = new Car("BMW", 1);
+        System.out.println(car1.equals(car2));
+        System.out.println(car1.hashCode());
+        System.out.println(car2.hashCode());
+    }
+
+}
